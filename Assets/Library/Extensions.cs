84f672362ec2d8b82c430d27ce401023434c6db8
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-06-18 19:23:53 +0300

    Extensions extended:D

--- Assets/Library/Extensions.cs
+++ Assets/Library/Extensions.cs
@@ -3,9 +3,127 @@ using System.Linq;
 using System.Collections.Generic;
 public static class IntExtensions
 {
+
     public static bool Multiples(this int a, int b)
     {
         return b != 0 && a % b == 0 && a != 0;
+    }
+
+
+    public static bool Even(this int a)
+    {
+        return a % 2 == 0;
+    }
+    public static bool EvenNo0(this int a)
+    {
+        return a % 2 == 0 && a != 0;
+    }
+
+
+    public static bool Whole(this int a)
+    {
+        return a % 1 == 0;
+    }
+    public static bool Whole(this short a)
+    {
+        return a % 1 == 0;
+    }
+    public static bool Whole(this float a)
+    {
+        return Mathf.Abs(a % 1) <= (double.Epsilon * 100);
+    }
+    public static bool Whole(this double a)
+    {
+        return Mathf.Abs((float)a % 1) <= (double.Epsilon * 100);
+    }
+    public static bool Whole(this decimal a)
+    {
+        return a % 1 == 0;
+    }
+
+
+    /// <summary>
+    /// Does the range of numbers between n and m (exclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InBounds(this int a, int n, int m)
+    {
+        return a > n && a < m;
+    }
+    /// <summary>
+    /// Does the range of numbers between n and m (exclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InBounds(this float a, float n, float m)
+    {
+        return a > n && a < m;
+    }
+    /// <summary>
+    /// Does the range of numbers between n and m (exclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InBounds(this double a, double n, double m)
+    {
+        return a > n && a < m;
+    }
+    /// <summary>
+    /// Does the range of numbers between n and m (exclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InBounds(this decimal a, decimal n, decimal m)
+    {
+        return a > n && a < m;
+    }
+    /// <summary>
+    /// Does the range of numbers between n and m (exclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InBounds(this short a, short n, short m)
+    {
+        return a > n && a < m;
+    }
+
+
+
+    /// <summary>
+    /// Does the range of numbers between n and m (inclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InOnBounds(this int a, int n, int m)
+    {
+        return a >= n && a <= m;
+    }
+    /// <summary>
+    /// Does the range of numbers between n and m (inclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InOnBounds(this float a, float n, float m)
+    {
+        return a >= n && a <= m;
+    }
+    /// <summary>
+    /// Does the range of numbers between n and m (inclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InOnBounds(this double a, double n, double m)
+    {
+        return a >= n && a <= m;
+    }
+    /// <summary>
+    /// Does the range of numbers between n and m (inclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InOnBounds(this decimal a, decimal n, decimal m)
+    {
+        return a >= n && a <= m;
+    }
+    /// <summary>
+    /// Does the range of numbers between n and m (inclusive) contain a?
+    /// </summary>
+    /// <returns>true if does, overwise false</returns>
+    public static bool InOnBounds(this short a, short n, short m)
+    {
+        return a >= n && a <= m;
     }
 }
 public static class RandomExtensions
@@ -35,8 +153,9 @@ public static class CollectionExtensions
     /// Finds index of first element equals format
     /// </summary>
     /// <param name="collection"></param>
+    /// <param name="format">defalt value. null or 0 or '' or "" etc.</param>
     /// <returns>index of first null element in array. If it is full than returns -1</returns>
-    public static int FirstEmpty(this object[] collection, object format = null)
+    public static int First(this object[] collection, object format = null)
     {
         for (int i = 0; i < collection.Length; i++)
             if (collection[i] == format)
@@ -49,5 +168,9 @@ public static class IEnumerableExtensions
     public static string ToStringFormat<T>(this IEnumerable<T> collection)
     {
         return string.Join(", ", collection);
+    }
+    public static string ToStringFormat<T>(this IEnumerable<T> collection, string separator)
+    {
+        return string.Join(separator, collection);
     }
 }
--- Assets/Library/Main.cs
+++ Assets/Library/Main.cs
@@ -4,8 +4,12 @@ public class Main : MonoBehaviour
 {
     void Start()
     {
-        //MonoBehaviourExt.extensions.Log(1, 2, "helo");
-        MonoBehaviourExt.extensions.LogColor("#00ffffff", "hello world");
-        MonoBehaviourExt.extensions.Log(MonoBehaviourExt.extensions.Loop<int[], int>(RandomExtensions.GetChance, new int[4] { 2, 4, 8, 16}, 100).ToStringFormat());
+        MonoBehaviourExt.extensions.Log(
+            MonoBehaviourExt.extensions.Loop<int[], int>(
+                RandomExtensions.GetChance, new int[4] { 2, 4, 8, 16}, 100).ToStringFormat(" "));
+
+        MonoBehaviourExt.extensions.Log(0.1f.Whole());
+        MonoBehaviourExt.extensions.Log(1.0f.Whole());
+        MonoBehaviourExt.extensions.Log(1.1f.Whole());
     }
 }
