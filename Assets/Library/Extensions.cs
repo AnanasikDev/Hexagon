90c88ca15a91aad21b544e07e8ae1c2757cfe226
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-06-21 14:25:08 +0300

    Cleared. Transforms ext. added

--- Assets/Library/Extensions.cs
+++ Assets/Library/Extensions.cs
@@ -1,131 +1,6 @@
 using UnityEngine;
 using System.Linq;
 using System.Collections.Generic;
-public static class IntExtensions
-{
-
-    public static bool Multiples(this int a, int b)
-    {
-        return b != 0 && a % b == 0 && a != 0;
-    }
-
-
-    public static bool Even(this int a)
-    {
-        return a % 2 == 0;
-    }
-    public static bool EvenNo0(this int a)
-    {
-        return a % 2 == 0 && a != 0;
-    }
-
-
-    public static bool Whole(this int a)
-    {
-        return a % 1 == 0;
-    }
-    public static bool Whole(this short a)
-    {
-        return a % 1 == 0;
-    }
-    public static bool Whole(this float a)
-    {
-        return Mathf.Abs(a % 1) <= (double.Epsilon * 100);
-    }
-    public static bool Whole(this double a)
-    {
-        return Mathf.Abs((float)a % 1) <= (double.Epsilon * 100);
-    }
-    public static bool Whole(this decimal a)
-    {
-        return a % 1 == 0;
-    }
-
-
-    /// <summary>
-    /// Does the range of numbers between n and m (exclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InBounds(this int a, int n, int m)
-    {
-        return a > n && a < m;
-    }
-    /// <summary>
-    /// Does the range of numbers between n and m (exclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InBounds(this float a, float n, float m)
-    {
-        return a > n && a < m;
-    }
-    /// <summary>
-    /// Does the range of numbers between n and m (exclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InBounds(this double a, double n, double m)
-    {
-        return a > n && a < m;
-    }
-    /// <summary>
-    /// Does the range of numbers between n and m (exclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InBounds(this decimal a, decimal n, decimal m)
-    {
-        return a > n && a < m;
-    }
-    /// <summary>
-    /// Does the range of numbers between n and m (exclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InBounds(this short a, short n, short m)
-    {
-        return a > n && a < m;
-    }
-
-
-
-    /// <summary>
-    /// Does the range of numbers between n and m (inclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InOnBounds(this int a, int n, int m)
-    {
-        return a >= n && a <= m;
-    }
-    /// <summary>
-    /// Does the range of numbers between n and m (inclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InOnBounds(this float a, float n, float m)
-    {
-        return a >= n && a <= m;
-    }
-    /// <summary>
-    /// Does the range of numbers between n and m (inclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InOnBounds(this double a, double n, double m)
-    {
-        return a >= n && a <= m;
-    }
-    /// <summary>
-    /// Does the range of numbers between n and m (inclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InOnBounds(this decimal a, decimal n, decimal m)
-    {
-        return a >= n && a <= m;
-    }
-    /// <summary>
-    /// Does the range of numbers between n and m (inclusive) contain a?
-    /// </summary>
-    /// <returns>true if does, overwise false</returns>
-    public static bool InOnBounds(this short a, short n, short m)
-    {
-        return a >= n && a <= m;
-    }
-}
 public static class RandomExtensions
 {
     public static int GetChance(int[] chances)
@@ -160,45 +35,55 @@ public static class RandomExtensions
         return randNormal;
     }
 }
-public static class CollectionExtensions
-{
-    public static object[] Add(this object[] a, object[] b)
-    {
-        object[] newCollection = new object[a.Length + b.Length];
-        for (int i = 0; i < a.Length; i++) newCollection[i] = a[i];
-        for (int i = a.Length; i < b.Length; i++) newCollection[i] = b[i];
-        return newCollection;
-    }
-    /// <summary>
-    /// Finds index of first element equals format
-    /// </summary>
-    /// <param name="collection"></param>
-    /// <param name="format">defalt value. null or 0 or '' or "" etc.</param>
-    /// <returns>index of first null element in array. If it is full than returns -1</returns>
-    public static int First(this object[] collection, object format = null)
-    {
-        for (int i = 0; i < collection.Length; i++)
-            if (collection[i] == format)
-                return i;
-        return -1;
-    }
-}
 public static class IEnumerableExtensions
 {
-    public static string ToStringFormat<T>(this IEnumerable<T> collection)
+    public static string Join<T>(this string separator, IEnumerable<T> collection)
     {
-        return string.Join(", ", collection);
+        return string.Join(separator, collection);
     }
-    public static string ToStringFormat<T>(this IEnumerable<T> collection, string separator)
+    public static string Join<T>(this IEnumerable<T> collection, string separator)
     {
         return string.Join(separator, collection);
     }
 }
 public static class GameObjectExtensions
 {
-    public static bool Has<T>(this GameObject gameObject, T type)
+    public static bool HasComponent<T>(this GameObject gameObject, T type)
     {
         T component;
         return gameObject.TryGetComponent<T>(out component);
+    }
+}
+public static class TransformExtensions
+{
+    public static void Reset(this Transform transform)
+    {
+        transform.ResetPosition();
+        transform.ResetRotation();
+        transform.ResetScale();
+    }
+    public static void ResetPosition(this Transform transform)
+    {
+        transform.position = Vector3.zero;
+    }
+    public static void ResetLocalPosition(this Transform transform)
+    {
+        transform.localPosition = Vector3.zero;
+    }
+    public static void ResetRotation(this Transform transform)
+    {
+        transform.rotation = Quaternion.identity;
+    }
+    public static void ResetLocalRotation(this Transform transform)
+    {
+        transform.localRotation = Quaternion.identity;
+    }
+    public static void ResetScale(this Transform transform)
+    {
+        transform.localScale = Vector3.one;
+    }
+    public static Vector3 globalScale(this Transform transform)
+    {
+        return transform.lossyScale;
     }
 }
--- Assets/Library/Main.cs
+++ Assets/Library/Main.cs
@@ -4,6 +4,17 @@ public class Main : MonoBehaviour
 {
     void Start()
     {
-        for (int i = 0; i < 100; i++) print(RandomExtensions.NormalDistribution(0, 10));
+        //for (int i = 0; i < 100; i++) print(RandomExtensions.NormalDistribution(0, 10));
+        print(Scale01(2, -5, 5));
+        
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) transform.Reset();
+    }
+    float Scale01(float n, float min, float max)
+    {
+        float m = n / (max - min);
+        return m;
     }
 }
