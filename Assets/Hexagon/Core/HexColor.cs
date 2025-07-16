5e07f8e863a2de5169837abbde3b09af4a5eab7b
Ananaseek <oleg.brovchenkov@gmail.com>
2025-07-16 23:23:56 +0200

    HexColor extended

--- Assets/Hexagon/Core/HexColor.cs
+++ Assets/Hexagon/Core/HexColor.cs
@@ -14,4 +14,62 @@ public static class ModHexColor
     /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3 ColorToVector(this Color color) => new Vector3(color.r, color.g, color.b);
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color WithRed(this Color color, float r) => new Color(r, color.g, color.b, color.a);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color WithGreen(this Color color, float g) => new Color(color.r, g, color.b, color.a);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color WithBlue(this Color color, float b) => new Color(color.r, color.g, b, color.a);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Color WithAlpha(this Color color, float a) => new Color(color.r, color.g, color.b, a);
+
+    public static float GetRGBMagnitude(this Color color)
+    {
+        return Mathf.Sqrt(color.r * color.r + color.g * color.g + color.b * color.b);
+    }
+
+    public static float GetRGBAMagnitude(this Color color)
+    {
+        return Mathf.Sqrt(color.r * color.r + color.g * color.g + color.b * color.b + color.a * color.a);
+    }
+
+    public static Color ScaleRGB(this Color color, float scalar)
+    {
+        return new Color(color.r * scalar, color.g * scalar, color.b * scalar, color.a);
+    }
+
+    public static Color ScaleRGBA(this Color color, float scalar)
+    {
+        return new Color(color.r * scalar, color.g * scalar, color.b * scalar, color.a * scalar);
+    }
+
+    /// <summary>
+    /// Divides RGB color by its length like a vector. Results in a color such that it does not depend on the input brightness. Output brightness is always the same. Output saturation differs from the input.
+    /// </summary>
+    public static Color NormalizeRGB(this Color color)
+    {
+        return ScaleRGB(color, 1.0f / color.GetRGBMagnitude());
+    }
+
+    /// <summary>
+    /// Divides RGBA color by its length like a vector. Results in a color such that it does not depend on the input brightness. Output brightness is always the same. Output saturation differs from the input.
+    /// </summary>
+    public static Color NormalizeRGBA(this Color color)
+    {
+        return ScaleRGBA(color, 1.0f / color.GetRGBAMagnitude());
+    }
+
+    public static string WrapInHTMLTagRGB(this Color color, string @string)
+    {
+        return $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{@string}</color>";
+    }
+    public static string WrapInHTMLTagRGBA(this Color color, string @string)
+    {
+        return $"<color=#{ColorUtility.ToHtmlStringRGBA(color)}>{@string}</color>";
+    }
 }
--- Assets/Hexagon/Examples/Example.cs
+++ Assets/Hexagon/Examples/Example.cs
@@ -1,51 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Pool;
 
 [RequireComponent(typeof(HexCoroutineRunner))]
 public class Example : MonoBehaviour
 {
-    Pool<MyPoolUnit> pool = new Pool<MyPoolUnit>(x => x.gameObject.activeSelf, () => MyPoolUnit.Create());
-    private Image image;
+    ObjectPool<GameObject> pool;
+
+    [SerializeField] private Transform pivot;
+    [SerializeField] private Transform point;
+    [SerializeField] private Color color;
+    private Color outcolor;
 
     private void Start()
     {
-        float[] segs = new float[] { 0.1f, 0.4f, 0.5f };
-        for (int i = 0; i < 10000; i++)
-        {
-            Debug.Log(HexRandom.GetWeightedIndex(segs, 1.0f));
-        }
-
-        //List<int> a = new List<int> { 1, 2, 3 };
-        //List<int> b = new List<int>(a);
-        //int[] c = new int[] { 10, 11, 12 };
-        //int[] d = (int[])c.Clone();
-        //a[0] = 5;
-        //c[0] = 200;
-        //Debug.Log(b[0]);
-        //Debug.Log(d[0]);
+        Debug.Log(color);
     }
 
     private void Update()
     {
+        outcolor = color.NormalizeRGB();
+        point.GetComponent<MeshRenderer>().material.color = outcolor;
+        Debug.Log(outcolor.WrapInHTMLTagRGB("color"));
+        Debug.Log(color.GetRGBMagnitude());
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            pool.TakeInactiveOrCreate().gameObject.SetActive(true);
+            pool.Get();
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if (pool.TryTakeActive(out var go))
-                go.gameObject.SetActive(false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            if (pool.TryTakeActive(out var go)){
-                pool.Unrecord(go);
-            }
+            pool.Get().gameObject.SetActive(true);
         }
     }
 }
